3ed31592043efee5cacd7c40fafe41b57d86c2ad
Yusinzxc <yusinzxc>
2021-06-01 20:56:31 +0800

    React

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Switch, Route } from "react-router-dom";
 
 import Home from './routes/home.js'
-
+import Page1 from './routes/page1.js'
 const App = () => {
   return(
     <div className="main">
       <Switch>
         <Route path='/home'>
+          <Home />
+        </Route>
+        <Route path='/page1'>
+          <Page1 />
+        </Route>
+        <Route path='/'>
           <Home />
         </Route>
       </Switch>
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { useHistory } from 'react-router-dom'
 const Home = () => {
+  let history = useHistory()
+  const page1 = () => {
+    history.push("/page1")
+  }
   return(
-    <h1>Hijdjdjdjdome</h1
+    <>
+    <h1>Home</h1>
+    <button onClick={page1}>Go to page</button>
+    </>
   )
 }
 
